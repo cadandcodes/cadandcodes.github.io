dab060720b662f5cd16006d75421a8a4087ab302
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-01-17 22:28:43 +0600

    Initial commit with Nuxt 3 setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-      <h1>Welcome to cadandcodes.github.io</h1>
-      <p>This is a Nuxt 3 static website deployed on GitHub Pages.</p>
-    </div>
-  </template>
-  
-  <script setup>
-  // Add your scripts here
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
+  <div>
+    <h1>Welcome to cadandcodes.github.io</h1>
+    <p>This is a Nuxt 3 static website deployed on GitHub Pages.</p>
+  </div>
+</template>
+
+<script setup>
+// Add your scripts here
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
