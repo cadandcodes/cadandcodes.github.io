b94ecca511557fd13989227d526f187322dd5878
md-anamul-hasan <anam.me2k12@gmail.com>
2025-01-17 19:35:25 +0600

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+export default {
+  target: 'static', // default is 'server'
+  router: {
+    base: '/cadandcodes.github.io/'
+  },
+  // other configurations
+  head: {
+    title: 'cadandcodes.github.io',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'My Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    // Global CSS
+  ],
+  plugins: [
+    // Plugins
+  ],
+  components: true,
+  buildModules: [
+    // Build modules
+  ],
+  modules: [
+    // Modules
+  ],
+  build: {
+    // Build configuration
+  }
+}
