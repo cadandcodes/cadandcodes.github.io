b8f7a889ac8503a83fb7dadfa8938f07b8892585
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-01-17 22:14:00 +0600

    0

--- app.vue
+++ app.vue
@@ -1,27 +1,6 @@
 <template>
-  <div id="app">
-    <ItemList />
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
-
-<script>
-import ItemList from './components/ItemList.vue';
-
-export default {
-  name: 'App',
-  components: {
-    ItemList
-  }
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
