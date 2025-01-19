bc5f553c323ea607e8bb5099413ac05efbf1aaea
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-01-19 14:28:08 +0600

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,5 @@
-// nuxt.config.ts
-import { defineNuxtConfig } from 'nuxt/config'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  app: {
-    baseURL: '/',
-    head: {
-      title: 'cad-and-codes',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'My Nuxt.js project' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
-    },
-  },
-  css: [
-    '~/assets/styles/main.css'
-  ],
-  plugins: [],
-  components: true,
-  modules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts',
-  ],
-  build: {}
+  devtools: { enabled: true }
 })
-
