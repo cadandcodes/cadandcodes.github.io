3cd9e992493e5badd0012d2084a029f8efa29ce3
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-01-19 14:19:10 +0600

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
-
-  // Deployment configuration for GitHub Pages
   app: {
-    baseURL: '/', // Replace with your GitHub Pages repository name
-
-    // Global page headers
+    baseURL: '/',
     head: {
-    title: 'cad-and-codes',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      title: 'cad-and-codes',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'My Nuxt.js project' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     },
   },
-  // Global CSS
   css: [
     '~/assets/styles/main.css'
   ],
-  // Plugins to run before rendering page
-  plugins: [
-    '~/plugins/axios.js'
-  ],
-  // Auto import components
+  plugins: [],
   components: true,
-  // Modules for dev and build (recommended)
   modules: [
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/axios',
     '@nuxtjs/google-fonts',
   ],
-  // Content module configuration
-  // Build Configuration
-  build: {
-    // You can extend webpack config here
-  }
+  build: {}
 })
-function defineNuxtConfig(config: {
-  compatibilityDate: string;
-  devtools: { enabled: boolean };
-  app: { baseURL: string; head: { title: string; meta: { charset?: string; name?: string; content?: string; hid?: string; }[]; link: { rel: string; type: string; href: string; }[]; }; };
-  css: string[];
-  plugins: string[];
-  components: boolean;
-  modules: (string | [string, { baseURL: string }])[];
-  build: {};
-}) {
-  return config;
-}
 
