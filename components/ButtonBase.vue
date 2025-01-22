dad0d0453631a14832e2c0e9f9c92cd5abab484a
md-anamul-hasan <anam.me2k12@gmail.com>
2025-01-22 15:33:11 +0600

    0

---
+++ components/ButtonBase.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- Base -->
+
+  <a
+    class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+    href="#"
+  >
+    Download
+  </a>
+</template>
---
+++ components/ButtonBorder.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- Border -->
+
+  <a
+    class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+    href="#"
+  >
+    Download
+  </a>
+</template>
